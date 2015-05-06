b6ef6f3c2e91cc3f027cecad037570c1d6669e67
AL27F13 <yannnet51@hotmail.com>
2015-05-06 14:08:03 -0500

    uno
    listado

--- js/acciones.js
+++ js/acciones.js
@@ -6,8 +6,29 @@ document.addEventListener("deviceready",function(){
 	 });//tap a btnvibrar
 	 
 	 $('#btnbeep').on('tap',function(){
-		 navigator.beep(1);
+		 navigator.notification.beep(1);
 	 });//tap a btnbeep
+	 
+	 
+	 $('#izquierda').on('swipeleft',function(){
+	 alert("barrio a la izquierda");
+     })//tap a izquierda
+	 
+	  $('#derecha').on('swiperight',function(){
+	 alert("barrio a la derecha");
+     })//tap a derecha
+	 
+	 document.addEventListener("pause", function(){
+		 $('#listado').append("<p> se pausó </p>")
+	 });
+	 
+	  document.addEventListener("resume", function(){
+		 $('#listado').append("<p> se reinició </p>")
+	 });
+	 
+	 $(window).on ('orientationchange',function(e){
+	 $('#listado').append("<p> orientacion:" + e.orientation + "</p>")
+	 });
 }); 
 });
 
